ecebcd20bbc203a884acd5aadaa317636f8701c5
Ratan Kumar <ratanrudra010@gmail.com>
2023-04-25 17:24:14 +0530

    html file added

---
+++ src/main/java/org/example/controller/EmployeeController.java
@@ -0,0 +1,12 @@
+package org.example.controller;
+
+import org.example.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+
+}
