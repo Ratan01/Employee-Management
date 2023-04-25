2ea830e1ab0c0fa5946a92b4ac7194ee7cb0b5f6
Ratan Kumar <ratanrudra010@gmail.com>
2023-04-25 10:03:50 +0530

    entity added

--- src/main/java/org/example/entity/Employee.java
+++ src/main/java/org/example/entity/Employee.java
@@ -1,2 +1,54 @@
-package org.example.entity;public class Employee {
+package org.example.entity;
+
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
